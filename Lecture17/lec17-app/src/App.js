5acb5dba8c49e4760e284015e09a580cee0d3878
shivang139 <shivangagrawal139@gmail.com>
2024-06-26 03:28:08 +0530

    React Work

--- Lecture17/lec17-app/src/App.js
+++ Lecture17/lec17-app/src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import {Component} from "react";
-
-class App extends Component {
+class App extends Component{
 
   name="cipherschools";
   render(){
-    return (
+    return(
       <>
       <div>
-        <h1>This is {this.name}</h1>
-        {/* <h1>This is CipherSchools</h1> */}
+        <h1>
+          This is {this.name}
+        </h1>
       </div>
       <div>
-        <p>This is a paragraph and name is:{this.name}</p>
+        <p>This is an paragraph and name is:{this.name}</p>
       </div>
       </>
-    );
+    )
   }
 }
 
